6f853efbc5a503214990f28dea45308eae77c734
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-18 22:22:24 +0200

    (#3) security: remove extensions

--- src/Paralax.Security/src/Extensions.cs
+++ src/Paralax.Security/src/Extensions.cs
@@ -4,14 +4,6 @@ namespace Paralax.Security
 {
     public static class Extensions
     {
-        public static IParalaxBuilder AddSecurity(this IParalaxBuilder builder)
-        {
-            builder.Services
-                .AddSingleton<IEncryptor, Encryptor>()
-                .AddSingleton<IHasher, Hasher>()
-                .AddSingleton<ISigner, Signer>();
-
-            return builder;
-        }
+       
     }
 }
