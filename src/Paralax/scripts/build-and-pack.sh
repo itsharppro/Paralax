27d6e092fe19640fc0239ac8ea2b605a2e997b75
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-18 23:16:58 +0200

    (#3) paralax: udpate certs

--- src/Paralax/scripts/build-and-pack.sh
+++ src/Paralax/scripts/build-and-pack.sh
@@ -12,23 +12,19 @@ PACKAGE_VERSION="1.0.$GITHUB_RUN_NUMBER"
 echo "Building and packing the Paralax library..."
 dotnet pack -c release /p:PackageVersion=$PACKAGE_VERSION --no-restore -o ./nupkg
 
-echo "Package created for branch $GITHUB_REF_NAME"
+PACKAGE_PATH="./nupkg/Paralax.$PACKAGE_VERSION.nupkg"
 
-case "$GITHUB_REF_NAME" in
-  "dev")
-    echo "Uploading Paralax package to NuGet..."
-    PACKAGE_PATH="./nupkg/Paralax.$PACKAGE_VERSION.nupkg"
-
-    if [ -f "$PACKAGE_PATH" ]; then
-      echo "Package found: $PACKAGE_PATH"
-      dotnet nuget push "$PACKAGE_PATH" -k "$NUGET_API_KEY" -s https://api.nuget.org/v3/index.json
-      echo "Package uploaded to NuGet."
-    else
-      echo "Error: Package $PACKAGE_PATH not found."
-      exit 1
-    fi
-    ;;
-  *)
-    echo "Not on main branch, skipping NuGet package upload."
-    ;;
-esac
+if [ -f "$PACKAGE_PATH" ]; then
+  echo "Signing NuGet package..."
+  dotnet nuget sign "$PACKAGE_PATH" \
+    --certificate-path "$CERTIFICATE_PATH" \
+    --timestamper http://timestamp.digicert.com \
+    --certificate-password "$CERTIFICATE_PASSWORD"
+  
+  echo "Uploading Paralax package to NuGet..."
+  dotnet nuget push "$PACKAGE_PATH" -k "$NUGET_API_KEY" -s https://api.nuget.org/v3/index.json
+  echo "Package uploaded to NuGet."
+else
+  echo "Error: Package $PACKAGE_PATH not found."
+  exit 1
+fi
