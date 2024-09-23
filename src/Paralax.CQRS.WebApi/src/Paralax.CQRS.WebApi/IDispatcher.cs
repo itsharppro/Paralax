e9f69dffbd3797cb8592213b67001170e63ca4fe
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-23 02:59:00 +0200

    (#42) web api scalar: add cqrs web api

--- src/Paralax.CQRS.WebApi/src/Paralax.CQRS.WebApi/IDispatcher.cs
+++ src/Paralax.CQRS.WebApi/src/Paralax.CQRS.WebApi/IDispatcher.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Input;
 using Paralax.CQRS.Events;
 using Paralax.CQRS.Queries;
+using Paralax.CQRS.Commands;
+
 
 namespace Paralax.CQRS.WebApi
 {
--- src/Paralax.CQRS.WebApi/src/Paralax.CQRS.WebApi/InMemoryDispatcher.cs
+++ src/Paralax.CQRS.WebApi/src/Paralax.CQRS.WebApi/InMemoryDispatcher.cs
@@ -1,38 +1,38 @@
-using System.Threading;
-using System.Threading.Tasks;
-using Paralax.CQRS.Commands;
-using Paralax.CQRS.Events;
-using Paralax.CQRS.Queries;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Paralax.CQRS.Commands;
+    using Paralax.CQRS.Events;
+    using Paralax.CQRS.Queries;
 
-namespace Paralax.CQRS.WebApi
-{
-    public class InMemoryDispatcher : IDispatcher
+    namespace Paralax.CQRS.WebApi
     {
-        private readonly ICommandDispatcher _commandDispatcher;
-        private readonly IEventDispatcher _eventDispatcher;
-        private readonly IQueryDispatcher _queryDispatcher;
-
-        public InMemoryDispatcher(ICommandDispatcher commandDispatcher, IEventDispatcher eventDispatcher,
-            IQueryDispatcher queryDispatcher)
+        public class InMemoryDispatcher : IDispatcher
         {
-            _commandDispatcher = commandDispatcher;
-            _eventDispatcher = eventDispatcher;
-            _queryDispatcher = queryDispatcher;
-        }
+            private readonly ICommandDispatcher _commandDispatcher;
+            private readonly IEventDispatcher _eventDispatcher;
+            private readonly IQueryDispatcher _queryDispatcher;
 
-        public Task SendAsync<T>(T command, CancellationToken cancellationToken) where T : class, ICommand
-        {
-            return _commandDispatcher.SendAsync(command, cancellationToken);
-        }
+            public InMemoryDispatcher(ICommandDispatcher commandDispatcher, IEventDispatcher eventDispatcher,
+                IQueryDispatcher queryDispatcher)
+            {
+                _commandDispatcher = commandDispatcher;
+                _eventDispatcher = eventDispatcher;
+                _queryDispatcher = queryDispatcher;
+            }
 
-        public Task PublishAsync<T>(T @event, CancellationToken cancellationToken = default) where T : class, IEvent
-        {
-            return _eventDispatcher.PublishAsync(@event, cancellationToken);
-        }
+            public Task SendAsync<T>(T command, CancellationToken cancellationToken) where T : class, ICommand
+            {
+                return _commandDispatcher.SendAsync(command, cancellationToken);
+            }
 
-        public Task<TResult> QueryAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default)
-        {
-            return _queryDispatcher.QueryAsync(query, cancellationToken);
+            public Task PublishAsync<T>(T @event, CancellationToken cancellationToken = default) where T : class, IEvent
+            {
+                return _eventDispatcher.PublishAsync(@event, cancellationToken);
+            }
+
+            public Task<TResult> QueryAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default)
+            {
+                return _queryDispatcher.QueryAsync(query, cancellationToken);
+            }
         }
     }
-}
