e84897a436cfbc082f1aa216b5a6d43021997426
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-24 18:27:50 +0200

    (#59) protobuf: update scripts

---
+++ src/Paralax.gRPC.Protobuf/scripts/test-and-collect-coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Running tests and collecting coverage for Paralax.gRPC.Protobuf..."
+
+cd src/Paralax.gRPC.Protobuf/tests/Paralax.gRPC.Protobuf
+
+echo "Restoring NuGet packages..."
+dotnet restore
+
+echo "Running tests and generating code coverage report..."
+dotnet test --collect:"XPlat Code Coverage" --results-directory ./TestResults
+
+# Check if tests succeeded
+if [ $? -ne 0 ]; then
+  echo "Tests failed. Exiting..."
+  exit 1
+fi
+
