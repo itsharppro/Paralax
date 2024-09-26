a175bc84743bf8e319ae68dfa0b33582049ecaea
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-27 00:51:36 +0200

    (#72) (#71) (#70) (#66)

---
+++ src/Paralax.Persistence.Redis/scripts/build-and-pack.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Executing post-success scripts for branch $GITHUB_REF_NAME"
+echo "Starting build and NuGet package creation for Paralax framework..."
+
+cd src/Paralax.Persistence.Redis/src/Paralax.Persistence.Redis
+
+echo "Restoring NuGet packages..."
+dotnet restore
+
+PACKAGE_VERSION="1.0.$GITHUB_RUN_NUMBER"
+echo "Building and packing the Paralax.Persistence.Redis library..."
+dotnet pack -c release /p:PackageVersion=$PACKAGE_VERSION --no-restore -o ./nupkg
+
+PACKAGE_PATH="./nupkg/Paralax.Persistence.Redis.$PACKAGE_VERSION.nupkg"
+
+if [ -f "$PACKAGE_PATH" ]; then
+  echo "Checking if the package is already signed..."
+  if dotnet nuget verify "$PACKAGE_PATH" | grep -q 'Package is signed'; then
+    echo "Package is already signed, skipping signing."
+  else
+    echo "Signing the NuGet package..."
+    dotnet nuget sign "$PACKAGE_PATH" \
+      --certificate-path "$CERTIFICATE_PATH" \
+      --timestamper http://timestamp.digicert.com 
+  fi
+
+  echo "Uploading Paralax.Persistence.Redis package to NuGet..."
+  dotnet nuget push "$PACKAGE_PATH" -k "$NUGET_API_KEY" \
+    -s https://api.nuget.org/v3/index.json --skip-duplicate
+  echo "Package uploaded to NuGet."
+else
+  echo "Error: Package $PACKAGE_PATH not found."
+  exit 1
+fi
