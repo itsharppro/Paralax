a1dbca524051ba6607f64e5a0968da9d58997cfb
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-28 12:25:42 +0200

    (#91) build: open api projects propertis target verions [pack-all-force]

--- src/Paralax.Docs.Swagger/src/Paralax.Docs.Swagger/Builders/SwaggerOptionsBuilder.cs
+++ src/Paralax.Docs.Swagger/src/Paralax.Docs.Swagger/Builders/SwaggerOptionsBuilder.cs
@@ -1,57 +1,84 @@
 namespace Paralax.Docs.Swagger.Builders
 {
-    internal sealed class SwaggerOptionsBuilder : ISwaggerOptionsBuilder
+    public class SwaggerOptionsBuilder : ISwaggerOptionsBuilder
     {
-        private readonly SwaggerOptions _options = new();
+        private string _title;
+        private string _version;
+        private string _routePrefix;
+
+        private bool _enabled;
+        private bool _reDocEnabled;
+        private string _name = string.Empty;
+        private bool _includeSecurity;
+        private bool _serializeAsOpenApiV2;
 
         public ISwaggerOptionsBuilder Enable(bool enabled)
         {
-            _options.Enabled = enabled;
+            _enabled = enabled;
             return this;
         }
 
         public ISwaggerOptionsBuilder ReDocEnable(bool reDocEnabled)
         {
-            _options.ReDocEnabled = reDocEnabled;
+            _reDocEnabled = reDocEnabled;
             return this;
         }
 
         public ISwaggerOptionsBuilder WithName(string name)
         {
-            _options.Name = name;
+            _name = name;
             return this;
         }
 
         public ISwaggerOptionsBuilder WithTitle(string title)
         {
-            _options.Title = title;
+            _title = title;
             return this;
         }
 
         public ISwaggerOptionsBuilder WithVersion(string version)
         {
-            _options.Version = version;
+            _version = version;
             return this;
         }
 
         public ISwaggerOptionsBuilder WithRoutePrefix(string routePrefix)
         {
-            _options.RoutePrefix = routePrefix;
+            _routePrefix = routePrefix;
             return this;
         }
 
         public ISwaggerOptionsBuilder IncludeSecurity(bool includeSecurity)
         {
-            _options.IncludeSecurity = includeSecurity;
+            _includeSecurity = includeSecurity;
             return this;
         }
 
         public ISwaggerOptionsBuilder SerializeAsOpenApiV2(bool serializeAsOpenApiV2)
         {
-            _options.SerializeAsOpenApiV2 = serializeAsOpenApiV2;
+            _serializeAsOpenApiV2 = serializeAsOpenApiV2;
             return this;
         }
 
-        public SwaggerOptions Build() => _options;
+        public SwaggerOptions Build()
+        {
+            if (string.IsNullOrWhiteSpace(_title) ||
+                string.IsNullOrWhiteSpace(_version) ||
+                string.IsNullOrWhiteSpace(_routePrefix))
+            {
+                throw new InvalidOperationException("All required properties must be set (Title, Version, RoutePrefix).");
+            }
+
+            var options = new SwaggerOptions(_title, _version, _routePrefix)
+            {
+                Enabled = _enabled,
+                ReDocEnabled = _reDocEnabled,
+                Name = _name,
+                IncludeSecurity = _includeSecurity,
+                SerializeAsOpenApiV2 = _serializeAsOpenApiV2
+            };
+
+            return options;
+        }
     }
 }
