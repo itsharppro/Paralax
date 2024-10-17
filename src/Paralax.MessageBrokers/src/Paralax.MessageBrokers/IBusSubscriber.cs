4d4a63a2d9246042111c1642dbdd886b4c7fec07
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-10-18 01:57:38 +0200

    (#126) messageBrokers: udpate subscriber/publisher interfaces

--- src/Paralax.MessageBrokers/src/Paralax.MessageBrokers/IBusSubscriber.cs
+++ src/Paralax.MessageBrokers/src/Paralax.MessageBrokers/IBusSubscriber.cs
@@ -3,5 +3,7 @@ namespace Paralax.MessageBrokers
     public interface IBusSubscriber
     {
         IBusSubscriber Subscribe<T>(Func<IServiceProvider, T, object, Task> handle) where T : class;
+
+        IBusSubscriber SubscribeToBroker<T>(Func<IServiceProvider, T, object, Task> handle, string brokerName) where T : class;
     }
 }
